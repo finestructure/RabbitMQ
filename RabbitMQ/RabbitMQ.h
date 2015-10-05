eb5d045fd83726352d5c8dca3da9acbe4d56bb28
Sven A. Schmidt <sas@feinstruktur.com>
2015-10-05 10:57:59 +0100

    Using quotes instead of <> for local imports

--- RabbitMQ/RabbitMQ.h
+++ RabbitMQ/RabbitMQ.h
@@ -14,9 +14,9 @@ FOUNDATION_EXPORT double RabbitMQVersionNumber;
 //! Project version string for RabbitMQ.
 FOUNDATION_EXPORT const unsigned char RabbitMQVersionString[];
 
-#import <amqp.h>
-#import <amqp_framing.h>
-#import <amqp_ssl_socket.h>
-#import <amqp_tcp_socket.h>
+#import "amqp.h"
+#import "amqp_framing.h"
+#import "amqp_ssl_socket.h"
+#import "amqp_tcp_socket.h"
 
 #import "amqp-ext.h"
