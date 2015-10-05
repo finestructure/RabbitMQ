d6f256aca0cc960b0863d9d7fbc59bb1facd9965
Sven A. Schmidt <sas@feinstruktur.com>
2015-10-05 10:01:46 +0100

    Initial Commit

---
+++ RabbitMQ/RabbitMQ.h
@@ -0,0 +1,19 @@
+//
+//  RabbitMQ.h
+//  RabbitMQ
+//
+//  Created by Sven A. Schmidt on 05/10/2015.
+//  Copyright © 2015 feinstruktur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RabbitMQ.
+FOUNDATION_EXPORT double RabbitMQVersionNumber;
+
+//! Project version string for RabbitMQ.
+FOUNDATION_EXPORT const unsigned char RabbitMQVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RabbitMQ/PublicHeader.h>
+
+
